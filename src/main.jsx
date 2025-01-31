4625a5ee360da9fbf483da030fc910dfabec1b51
Ashrafpoless <150967649+Ashrafpoless@users.noreply.github.com>
2025-01-31 23:29:49 +0100

    react-router-dom-pages

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { StrictMode } from 'react'
+import StoreContextProvider from './context/StoreContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+    <StrictMode>
+        <StoreContextProvider>
+            <App />
+        </StoreContextProvider>
+    </StrictMode>
+)
